118959ca11e1217bb07694cd8d41ce811de47c08
Ethan Watson <ethanwatson@Ethans-MacBook-Pro.local>
2020-08-04 20:56:16 -0500

    style clean up cigna

--- pages/cigna-oscar.js
+++ pages/cigna-oscar.js
@@ -17,38 +17,51 @@ class CignaOscar extends React.Component {
           <main>
             <Navbar name="fdaf"/>
 
-            <div className="head">
+            <div className="head cigna">
               <h2>Cigna + Oscar</h2>  
             </div>
 
             <h1 className="title">
-              Introducing Cigna + Oscar new health care partnership for Tennessee
+              Introducing Cigna + Oscar, a new health care partnership for Tennessee.
             </h1>
 
-            <h3>Combining Cigna's extensive provider network with Oscar's technical expertise</h3>
-            <h3>Very competitive rates</h3>
-            <h3>Choose from 12 different plans</h3>
-            <h3>Can cover groups from 2 to 50 employees</h3>
-            <h3>Guarantee issue</h3>
-            <h3>Pre-existing conditions covered immediately</h3>
-            <h3>Doctor on call (Virtual Urgent care) 24/7</h3>
-            <h3>Dedicated concierge team that will help find the right doctors, 
-            facilities, and manage your care</h3>
+            <h3>
+              <ul>
+                <li>Combining Cigna's extensive provider network with Oscar's technical expertise </li>
+                <li>Very competitive rates</li>
+                <li>Choose from 12 different plans</li>
+                <li>Can cover groups from 2 to 50 employees</li>
+                <li>Guarantee issue</li>
+                <li>Pre-existing conditions covered immediately</li>
+                <li>Doctor on call (Virtual Urgent care) 24/7</li>
+                <li>Dedicated concierge team that will help find the right doctors, 
+                facilities, and manage your care</li>
+              </ul>
+            </h3>
 
             <br />
 
-            <h2>Special Covid-19 assistance</h2>
+            <h2 className="cigna-section-title">Special Covid-19 assistance</h2>
 
-            <h3>Doctors on call 24/7 at no charge</h3>
-            <h3>Testing at no charge</h3>
-            <h3>Assistance finding testing sites</h3>
-            <h3>No charge for treatment if treating by an in network provider</h3>
+            <h3>
+              <ul>
+                <li>Doctors on call 24/7 at no charge</li>
+                <li>Testing at no charge</li>
+                <li>Assistance finding testing sites</li>
+                <li>No charge for treatment if treating by an in network provider</li>
+              </ul>
+            </h3>
 
-            <h2>For more information or to get a quote call or email: </h2>
-            <h3>Stepher Lester (901) 268-4824</h3>
-            <h4>Email - steve@fhmemphis.com</h4>
-            <h3>David Deaderick (901) 260-6420</h3>
-            <h4>Email - david@fhmemphis.com</h4>
+            <h2 className="cigna-section-title">For more information or to get a quote call or email: </h2>
+
+            <h3>
+              <ul>
+                <li><b>Stepher Lester (901) 268-4824</b></li>
+                <li>Email - steve@fhmemphis.com</li>
+                <li><b>David Deaderick (901) 260-6420</b></li>
+                <li>Email - david@fhmemphis.com</li>
+              </ul>
+            </h3>
 
             <ContactUs />
 
