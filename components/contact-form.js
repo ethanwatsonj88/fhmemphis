18d06c740239e606ec81a6d3b0e6a9514a072628
Ethan Watson <ethanwatson@Ethans-MacBook-Pro.local>
2020-08-09 16:05:45 -0500

    small ui updates

--- components/contact-form.js
+++ components/contact-form.js
@@ -22,14 +22,26 @@ export default function ContactUs() {
 
   return (
     <div className="contact-form" id="contact-form">
-      <h2><em>Ready to get started?</em> <br />
-      Send us your name and email, and we'll have David or Steve contact you.</h2>
+      <h2 className="contact-title">Ready to get started?</h2>
+      <p className="contact-description">Send us your name and email, and we'll have David or Steve contact you.</p>
       <form className="" onSubmit={sendEmail}>
         <input className="contact-form-input" type="hidden" name="contact_number" />
-        <label className="contact-form-label">Your Name:&nbsp;</label>
-        <input className="contact-form-input" type="text" name="user_name" />
-        <label className="contact-form-label">Your Email:&nbsp;</label>
-        <input className="contact-form-input" type="email" name="user_email" />
+        
+        <label className="contact-form-label">Business Name:&nbsp;</label>
+        <input className="contact-form-input" type="text" name="business_name" />
+        
+        <label className="contact-form-label">Business ZIP code:&nbsp;</label>
+        <input className="contact-form-input" type="text" name="zip_code" />
+        
+        <label className="contact-form-label">Number of employees:&nbsp;</label>
+        <input className="contact-form-input" type="text" name="num_employees" />
+
+        <label className="contact-form-label">Business Email:&nbsp;</label>
+        <input className="contact-form-input" type="email" name="business_email" />
+
+        <label className="contact-form-label">Desired start date:&nbsp;</label>
+        <input className="contact-form-input" type="date" name="start_date" />
+
         <label className="contact-form-label">A good time to contact you (Optional)&nbsp;</label>
         <input className="contact-form-input" type="text" name="message" />
         <input type="submit" value="Send" />
